ea7fc55c33d6ec48ac713edcc9e69615625f2d84
aliciacisnerosm <aliciacisnerosm@gmail.com>
2019-06-10 14:59:07 -0500

    Reset password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
+   
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -22,8 +23,6 @@ class User < ApplicationRecord
     def User.new_token
       SecureRandom.urlsafe_base64
     end
-  
-   
 
     # Remembers a user in the database for use in persistent sessions.
     def remember
@@ -53,6 +52,16 @@ class User < ApplicationRecord
       self.activation_digest = User.digest(activation_token)
     end
     
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest,  User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+    
     def activate
       update_attribute(:activated,    true)
       update_attribute(:activated_at, Time.zone.now)
@@ -62,4 +71,6 @@ class User < ApplicationRecord
     def send_activation_email
       UserMailer.account_activation(self).deliver_now
     end
+
+
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) } 
   it { should validate_presence_of(:email)}
   
-  it {is_expected.to validate_length_of(:name).is_at_least(10).on(:create)}
+  it {is_expected.to validate_length_of(:name).is_at_least(2).on(:create)}
 
   it "is valid with email as lower-case" do
     user2 = build(:user, email: "OLA")
